3b016ab4bf7f7e906a68d5d6e2a5cc9540164f5a
nik <nik@Nikolajs-MacBook-Pro.local>
2023-03-06 17:57:22 -0500

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h1>About Page</h1>
+    </>
+  )
+}
+
+export default AboutPage
